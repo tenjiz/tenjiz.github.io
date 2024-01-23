3c4ac2f7c3cde62b21fedad683cef0c6605e207b
tenjiz <140896202+tenjiz@users.noreply.github.com>
2024-01-23 15:22:19 +0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+document.body.innerHTML = '';
+
+var img = document.createElement('img');
+img.src = 'https://png.pngitem.com/pimgs/s/49-497525_annoyed-peter-peter-family-guy-transparent-hd-png.png';
+
+document.body.appendChild(img);
